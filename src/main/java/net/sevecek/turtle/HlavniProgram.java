c0c116184f9388700fadf33a8e121c72de6f5847
martinalangova <martina.langova@gmail.com>
2022-04-13 19:56:25 +0200

    více domečků

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -10,6 +10,8 @@ public class HlavniProgram {
         zofka = new Turtle();
         zofka.turnRight(180);
         zofka.move(100);
+        for(int i=0;i<3;i++) {
+
         zofka.turnLeft(90);
         zofka.move(100);
         zofka.turnLeft(90);
@@ -20,8 +22,11 @@ public class HlavniProgram {
         zofka.move(75);
         zofka.turnRight(90);
         zofka.move(75);
-
-
+        zofka.turnRight(45);
+        zofka.penUp();
+        zofka.move(100);
+        zofka.penDown();
+        }
 
 
     }
